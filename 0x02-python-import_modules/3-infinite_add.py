26afe9c7d1da1be5da304a458c8b9c1bd6fe9dc3
Matlo21 <mokomanematlotlo21@gmail.com>
2022-09-22 17:33:50 +0000

    fix

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,10 +2,8 @@
 from sys import argv
 
 if __name__ == "__main__":
-      newsum = 0
-
-if len(argv) > 1:
-      for i in range(1, len(argv)):
-          newsum += int(argv[i])
-
-print(newsum)
+    newsum = 0
+    if len(argv) > 1:
+        for i in range(1, len(argv)):
+            newsum += int(argv[i])
+    print(newsum)
