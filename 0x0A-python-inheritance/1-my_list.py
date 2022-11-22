e442e1a8c39b3ea12e7e635b781950cc4cb3ca56
Matlo21 <mokomanematlotlo21@gmail.com>
2022-11-22 09:23:54 +0000

    fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""Defines a MyList class that inherits from list"""
+"""Module that defines a MyList class that inherits from list"""
 
 
 class MyList(list):
-    """Class that inherits from list"""
+    """a subclass of list"""
+
+    def __init__(self):
+        """initializes the object"""
+
+        super().__init__()
 
     def print_sorted(self):
-        """Function that prints the list but sorted
-        """
+        """prints the sorted list"""
 
         print(sorted(self))
--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-"""Module that holds the class BaseGeometry
-"""
+"""Module that holds the class BaseGeometry"""
 
 
 class BaseGeometry:
-    """Class that defines a shape
-    """
-
-    pass
+    """Reprsent base geometry."""
 
     def area(self):
-        """Function that calculates the area of a shapre
-        """
+        """Not yet implemented."""
 
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Function that validates input
+        """Validate a parameter as an integer.
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
         """
 
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+            raise ValueError("{} must be greater than 0".format(name))
